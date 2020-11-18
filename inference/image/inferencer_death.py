2abf1220276823c8e2dd1245d7bd02d61a473700
German Telmo Eizaguirre <gteizaguirre@gmail.com>
2020-11-18 09:59:56 +0100

    inferencer serverless functions

---
+++ inference/image/inferencer_death.py
@@ -0,0 +1,41 @@
+import json
+import pickle
+import ibm_boto3
+import ibm_botocore
+import sklearn
+import pandas as pd
+import sys
+
+def main(args) :
+	msg="Null"
+
+	keys = [ "sex", "patient_type", "intubed", "pneumonia", "age", "pregnancy", "diabetes", "copd", "asthma", "inmsupr", "hypertension", "other_disease", "cardiovascular", "obesity", "renal_chronic", "tobacco", "contact_other_covid", "covid_res", "icu" ]
+
+	keys = list(set(keys) & set(args.keys()))
+	args = { key:args[key] for key in keys }
+
+	features_dict = { key:[value] for key, value in args.items() }
+	user_profile = pd.DataFrame(features_dict)
+
+	print(ibm_boto3.__version__)
+
+	config = {
+		'ibm_api_key_id' : 'HqjZ-aY5oQfrkI0cwNTMRMVTTRDv6JY-i6tCswcpiGvq',
+		'ibm_service_instance_id' : '584a3f8b-4540-4c26-a6ac-cc319cc06c9e',
+		'endpoint_url' : 'https://s3.private.eu.cloud-object-storage.appdomain.cloud',
+		'Bucket' : 'nextlevelhackathon2020-donotdelete-pr-s9nsxj5fxjzk0j'
+	}
+
+	ibm_cos = ibm_boto3.resource("s3",
+	             ibm_api_key_id=config['ibm_api_key_id'],
+	             ibm_service_instance_id=config['ibm_service_instance_id'],
+	             config=ibm_botocore.client.Config(signature_version="oauth"),
+	             endpoint_url=config['endpoint_url'])
+
+	stream_death = ibm_cos.Object(config['Bucket'], 'model_death.pickle').get()['Body']
+	model_death = pickle.loads(stream_death.read())
+
+	death_value = model_death.predict(user_profile)[0]
+
+	# return {"body":"<html><body><h3>{}</h3></body></html>".format(death_value)}
+	return { "body": { "death_value" : death_value }}
