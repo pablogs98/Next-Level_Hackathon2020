751ff5a4cc61a5ac6162e7b0ab7663263618745d
Pablo Gimeno <pablo.gimeno@urv.cat>
2020-11-18 11:52:43 +0100

    Form submit

--- frontend/form-validation.js
+++ frontend/form-validation.js
@@ -1,18 +1,23 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
-function send() {
-    'use strict'
+(function () {
+  'use strict'
 
+  window.addEventListener('load', function () {
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.getElementsByClassName('needs-validation')
-    var correct = false
+      var correct = true
 
     // Loop over them and prevent submission
     Array.prototype.filter.call(forms, function (form) {
+      form.addEventListener('submit', function (event) {
         if (form.checkValidity() === false) {
+          event.preventDefault()
+          event.stopPropagation()
+            correct = correct && false
         }
 
         form.classList.add('was-validated')
-    })
+      }, false)
 
     if (correct === true) {
         let age = $('#age').val();
@@ -128,7 +133,8 @@ function send() {
             true);
         xhttp.setRequestHeader("Content-type", "application/json");
         xhttp.send(JSON.stringify(json));
-
-        # todo redirect a api
     }
-}
+    })
+  }, false)
+})()
+
